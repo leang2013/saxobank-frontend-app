5022142ddcee6503afaae4e57b75dadabb51d582
Leandro <leang.martinez@gmail.com>
2020-06-08 10:52:00 +0200

    fix test

--- src/tests/components/orderBook.test.js
+++ src/tests/components/orderBook.test.js
@@ -5,9 +5,9 @@ import OrderBook from '../../components/orderBook';
 describe('component', () => {
   it('Should contain the number 2512', () => {
     const mockBids = [['0.02512000', '7.00000000']];
-    const mockAsks = [['0.02518000', '7.00000000']];
+    const mockAsks = [['0.02518000', '8.00000000']];
     const { getByText } = render(<OrderBook bids={mockBids} asks={mockAsks} />);
-    const linkElement = getByText(/2512/i);
+    const linkElement = getByText(/Spread/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
